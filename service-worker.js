e018a007e069de0982e3a43dd25d146c40b3cc3c
Seomar-lima <seomarlima2@gmail.com>
2025-06-16 14:21:28 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,48 @@
+const CACHE_NAME = "foto-showfest-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./moldura.png",
+  "./galeria.png",
+  "./beep.mp3",
+  "./icon_192x192.png",
+  "./icon_512x512.png",
+  "./screenshot1.jpg",
+  "https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js"
+];
+
+// Instalação: cacheia arquivos essenciais
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("foto-showfest-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./manifest.json",
-        "./moldura.png",
-        "./galeria.png",
-        "./beep.mp3",
-        "./icon_192x192.png",
-        "./icon_512x512.png",
-        "./screenshot1.jpg",
-        "https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
+// Ativação: limpa caches antigos
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: serve do cache se possível, senão busca online
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response =>
+      response || fetch(event.request).catch(() =>
+        caches.match("./index.html")
+      )
+    )
   );
 });
