91792b78500f8d0db6b38f9cacad3f1de12e1b5d
Seomar-lima <seomarlima2@gmail.com>
2025-06-16 00:02:01 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,12 @@
-self.addEventListener("install", event => { event.waitUntil( caches.open("foto-showfest-v1").then(cache => { return cache.addAll([ "./", "./index.html", "./style.css", "./script.js", "./manifest.json", "./moldura.png", "./galeria.png", "./beep.mp3", "./icon_192x192.png", "./icon_512x512.png", "https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js" ]); }) ); });
+self.addEventListener("install", event =>
+  { event.waitUntil( caches.open("foto-showfest-v1").then(cache => 
+    { return cache.addAll([ "./", "./index.html", "./style.css", 
+    "./script.js", "./manifest.json",
+     "./moldura.png", "./galeria.png",
+      "./beep.mp3", "./icon_192x192.png", 
+       "./icon_512x512.png",
+       "https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js" ]); }) ); });
 
-self.addEventListener("fetch", event => { event.respondWith( caches.match(event.request).then(response => { return response || fetch(event.request); }) ); });
+self.addEventListener("fetch", event =>
+  { event.respondWith( caches.match(event.request).then(response =>
+  { return response || fetch(event.request); }) ); });
