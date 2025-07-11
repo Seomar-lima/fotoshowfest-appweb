9afd223cc7b24ef598b92cc0fabea82c063253f3
Seomar-lima <seomarlima2@gmail.com>
2025-07-11 11:14:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -150,6 +150,7 @@ function gerarQRCode(link) {
     colorLight: "#ffffff",
     correctLevel: QRCode.CorrectLevel.H
   });
+}
 
   const downloadLink = document.createElement("a");
   downloadLink.href = link;
