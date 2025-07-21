ec97db7ca9ca0debc00464dcc0d307e92549ef0f
Seomar-lima <seomarlima2@gmail.com>
2025-07-20 21:36:57 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,26 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = "foto-show-fest-cache-v1";
+const urlsToCache = [
+  "./",
+  "index.html",
+  "style.css",
+  "script.js",
+  "moldura.png",
+  "beep.mp3",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("foto-showfest-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./manifest.json",
-        "./moldura.png",
-        "./galeria.png",
-        "./beep.mp3",
-        "./icon_192x192.png",
-        "./icon_512x512.png",
-        "./screenshot1.jpg",
-        "https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
