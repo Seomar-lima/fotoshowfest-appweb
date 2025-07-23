da3db7eb5e95a7cef209e80505bb8e4264532d42
Seomar-lima <seomarlima2@gmail.com>
2025-07-23 14:17:46 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,27 @@
-const CACHE_NAME = "foto-show-fest-cache-v2";
+const CACHE_NAME = "foto-show-fest-cache-v1";
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/moldura.png',
-  '/beep.mp3',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+  "./",
+  "index.html",
+  "style.css",
+  "script.js",
+  "moldura.png",
+  "beep.mp3",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => Promise.all(
-      cacheNames.map((cacheName) => {
-        if (!cacheWhitelist.includes(cacheName)) {
-          return caches.delete(cacheName);
-        }
-      })
-    ))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
